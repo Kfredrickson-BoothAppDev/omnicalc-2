072c8b66165db6377f53b7fccdca2dd542bebfaf
Kyle Fredrickson <kyle.fredrickson@chicagobooth.edu>
2023-04-11 16:37:00 +0000

    graded portion final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ :template => "calculation_templates/add.html.erb"})
+  end
+  def wizard_add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
+    render({ :template => "calculation_templates/wizard_add.html.erb"})
+  end 
 
+  def subtract
+    render({ :template => "calculation_templates/subtract.html.erb"})
+  end
+  def wizard_subtract
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num - @second_num
+    render({ :template => "calculation_templates/wizard_subtract.html.erb"})
+  end
+
+  def multiply
+    render({ :template => "calculation_templates/multiply.html.erb"})
+  end
+  def wizard_multiply
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num * @second_num
+    render({ :template => "calculation_templates/wizard_multiply.html.erb"})
+  end
+
+  def divide
+    render({ :template => "calculation_templates/divide.html.erb"})
+  end
+  def wizard_divide
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
+    render({ :template => "calculation_templates/wizard_divide.html.erb"})
+  end
 end
